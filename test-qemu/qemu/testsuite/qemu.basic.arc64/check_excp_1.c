698818a00c9864a33540f62bae1bd774496e34f9
Luis Silva <luis.m.silva99@hotmail.com>
2023-08-08 19:44:10 +0100

    Integrated the QEMU testsuite port for DejaGNU.
    Validated each ported test to identify and address potential
    outdated issues.
    Enriched the QEMU testsuite with newly introduced TCG tests.
    
    Signed-off-by: Luis Silva <luis.m.silva99@hotmail.com>

---
+++ test-qemu/qemu/testsuite/qemu.basic.arc64/check_excp_1.c
@@ -0,0 +1,17 @@
+/*  { dg-skip-if "" { arc-*-elf* arc32-*-elf* } } */
+
+#include <stdio.h>
+
+void main (void)
+{
+  __builtin_arc_trap_s (0);
+  printf ("[PASS] TRAPC:1\n");
+  __builtin_arc_trap_s (1);
+  printf ("[PASS] TRAPC:2\n");
+}
+
+void __attribute__ ((interrupt("ilink")))
+EV_Trap (void)
+{
+  printf ("[PASS] TRAPC:IRQ\n");
+}
